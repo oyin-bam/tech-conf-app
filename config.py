46a9ec0e023e643d2faa5776eb557d5d66da2b86
oyin <oyindaakinyemi@gmail.com>
2022-07-27 05:18:07 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="techconf-db-server.postgres.database.azure.com"
+    POSTGRES_USER="bambam@techconf-db-server"
+    POSTGRES_PW="Akinyemi1#"
+    POSTGRES_DB="techconfdb"
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconf-servicebus.servicebus.windows.net/;SharedAccessKeyName=RootManagedSharedAccessKey;SharedAccessKey=pRXSa0b/dIMEY9Mi3oJFlcL/pPSshmzevXW5gQ/+FP8=;EntityPath=notificationqueue'
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
+    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
